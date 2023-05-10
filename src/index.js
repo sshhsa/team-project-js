61dc21044d1af3526d1e633abfd095462a4bf385
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-10 18:09:37 +0300

    Main set up

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/tumbler-theme';
+
+import './js/auth-modal';
+import './js/categories';
+import './js/gallery-categories';
+import './js/modal-item-home';
+import './js/support-ukraine';
---
+++ src/shop-list.js
@@ -0,0 +1,4 @@
+import './js/header';
+import './js/tumbler-theme';
+import './js/support-ukraine';
+import './js/shoplist-gallery-categories';
