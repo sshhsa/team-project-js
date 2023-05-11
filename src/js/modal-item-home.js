811d0e9108c410c21b97a8bd3cf9971dfb0603c6
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-11 12:27:03 +0300

    commit

--- src/js/modal-item-home.js
+++ src/js/modal-item-home.js
@@ -3,20 +3,20 @@ const refsBtnOpen = document.querySelectorAll('.js-open');
 const btnClose = document.querySelector('.js-close');
 const refModal = document.querySelector('.backdrop');
 
-btnClose.addEventListener('click', onBtnClose)
+btnClose.addEventListener('click', onBtnClose);
 
 for (let refBtnOpen of refsBtnOpen) {
   refBtnOpen.addEventListener('click', onClickBtn);
 }
 
-function onClickBtn(e){
-  e.preventDefault()
-  refModal.classList.add('is-block')
+function onClickBtn(e) {
+  e.preventDefault();
+  refModal.classList.add('is-block');
 }
 
-function onBtnClose(e){
-  e.preventDefault()
-    refModal.classList.remove('is-block')
+function onBtnClose(e) {
+  e.preventDefault();
+  refModal.classList.remove('is-block');
 }
 
 //================================КОД ДЛЯ ВІДКРИТТЯ ТА ЗАКРИТЯ МОДАЛЬНОГО ВІКНА REMOVE=============================================
@@ -24,18 +24,18 @@ const refsBtnOpenRemove = document.querySelectorAll('.js-open-remove');
 const btnCloseRemove = document.querySelector('.js-close-remove');
 const refModalRemove = document.querySelector('.backdrop-remove');
 
-btnCloseRemove.addEventListener('click', onBtnClose)
+btnCloseRemove.addEventListener('click', onBtnClose);
 
 for (let refBtnOpenRemove of refsBtnOpenRemove) {
   refBtnOpenRemove.addEventListener('click', onClickBtn);
 }
 
-function onClickBtn(e){
-  e.preventDefault()
-  refModalRemove.classList.add('is-block')
+function onClickBtn(e) {
+  e.preventDefault();
+  refModalRemove.classList.add('is-block');
 }
 
-function onBtnClose(e){
-  e.preventDefault()
-    refModalRemove.classList.remove('is-block')
+function onBtnClose(e) {
+  e.preventDefault();
+  refModalRemove.classList.remove('is-block');
 }
