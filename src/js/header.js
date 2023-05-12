6e40d88d47964ae85a3a7d99c27bd648f8ebd6eb
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-05-13 01:48:53 +0300

    header, footer, jsheader,burger

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,11 @@
-console.log('HEADER!!!');
+const openMenuBtn = document.querySelector('.menu-burger-open');
+const closeMenuBtn = document.querySelector('.mobile-menu-close-button');
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+
+openMenuBtn.addEventListener('click', () => {
+  mobileMenuContainer.classList.add('is-open');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  mobileMenuContainer.classList.remove('is-open');
+});
