7ec52f1733548c87d74d04ef8f3e35dbc387c40f
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-05-16 14:20:02 +0300

    refact active category

--- src/js/categories.js
+++ src/js/categories.js
@@ -10,117 +10,117 @@ const galleryTitle = document.querySelector('.gallery-title');
 let varWithCurrentCategoryValue = 'ALL CATEGORIES';
 const bookApi = getCategoryList();
 
-
-getCategoryList()
-.then((categorie) => {
-    if (!categorie.length) {
-        Notify.failure("Oops something going wrong.");
-        return;
-    }
-    filterListEl.insertAdjacentHTML('beforeend', createMarkup(categorie));;
+getCategoryList().then(categorie => {
+  if (!categorie.length) {
+    Notify.failure('Oops something going wrong.');
+    return;
+  }
+  filterListEl.insertAdjacentHTML('beforeend', createMarkup(categorie));
 });
 
-
-function createMarkup (arr){
-    return arr.map(value => `<li class="filter__item" data-mark-active="${value.list_name}">${value.list_name}</li>`).join('');
- }
+function createMarkup(arr) {
+  return arr
+    .map(
+      value =>
+        `<li class="filter__item" data-mark-active="${value.list_name}">${value.list_name}</li>`
+    )
+    .join('');
+}
 
 filterListEl.addEventListener('click', event => {
-    
-    if (event.target.outerText.toLowerCase() === varWithCurrentCategoryValue.toLowerCase()) {
-      return;
-    }
-  
-    varWithCurrentCategoryValue = event.target.outerText;
-  
-    addGalleryMarkupAndChangeFilter();
+  if (
+    event.target.outerText.toLowerCase() ===
+    varWithCurrentCategoryValue.toLowerCase()
+  ) {
+    return;
+  }
 
-    
-    if (event.target.textContent === "All categories") {
-      location.reload();
-      return;
-      }
-    addCardsByCategory();
-  });
-  
-  function addGalleryMarkupAndChangeFilter() {
-    const targetEl = document.querySelector(`[data-mark-active="${varWithCurrentCategoryValue}"]`);
-  
-    varWithActiveValueFilter.classList.remove('filter__item--active');
-  
-    targetEl.classList.add('filter__item--active');
-  
-    varWithActiveValueFilter = targetEl;
-  } 
+  varWithCurrentCategoryValue = event.target.outerText;
 
+  addGalleryMarkupAndChangeFilter();
 
+  if (event.target.textContent === 'All categories') {
+    location.reload();
+    return;
+  }
+  addCardsByCategory();
+});
 
+function addGalleryMarkupAndChangeFilter() {
+  const targetEl = document.querySelector(
+    `[data-mark-active="${varWithCurrentCategoryValue}"]`
+  );
 
+  varWithActiveValueFilter.classList.remove('filter__item--active');
 
+  targetEl.classList.add('filter__item--active');
 
-
-
-
-
+  varWithActiveValueFilter = targetEl;
+}
+console.log(varWithActiveValueFilter);
 
 //// Рендер карток книжок по категоріям
-function addCardsByCategory() { 
+function addCardsByCategory() {
   const categoryValue = varWithActiveValueFilter.innerHTML;
-  console.log(categoryValue) 
-  getBooksCategory(categoryValue).then((booksArr) => {
-     if (!booksArr.length) {
-         Notify.failure(`Sorry, there are no ${categoryValue} books. Please choose another category.`);
-         return;
-     }
-     
-     galleryTitle.innerHTML = categoryValue;
-     booksList.innerHTML = createMoreBooks(booksArr);
+  console.log(categoryValue);
+  getBooksCategory(categoryValue).then(booksArr => {
+    if (!booksArr.length) {
+      Notify.failure(
+        `Sorry, there are no ${categoryValue} books. Please choose another category.`
+      );
+      return;
+    }
 
-     addColorToTitle();
-     addModal();
+    galleryTitle.innerHTML = categoryValue;
+    booksList.innerHTML = createMoreBooks(booksArr);
+
+    addColorToTitle();
+    addModal();
 
     //  bookGalleryCard.forEach((book) => {
     //    book.addEventListener('click', (e) => {
     //      const bookId = e.currentTarget.id;
     //      modalOpen(bookId);
-    //    }) 
+    //    })
     //  })
- })
+  });
 }
 
 //// Створення карток книжок по категоріям
 function createMoreBooks(booksArr) {
- const bookCard =  booksArr.map(({_id, book_image, title, author}) => {
-     //  Перевірка чи пришла обложка книги з бекенду і заміна її на заглушку в разі необхідності //
-     if (!book_image) {
-         book_image = "../images/book_plug.jpg";
-     }
+  const bookCard = booksArr
+    .map(({ _id, book_image, title, author }) => {
+      //  Перевірка чи пришла обложка книги з бекенду і заміна її на заглушку в разі необхідності //
+      if (!book_image) {
+        book_image = '../images/book_plug.jpg';
+      }
 
-     const markup = `<li id="${_id}" class="books-gallery__card">
+      const markup = `<li id="${_id}" class="books-gallery__card">
      <div class="card-container">
      <img class="books-gallery__card-img" src="${book_image}" alt="${title}" loading="lazy">
      <div class="port-overlay"><p>quick view</p></div>
      </div>
      <h2 class="books-gallery__card-title">${title}</h2>
      <p class="books-gallery__card-author">${author}</p>
-     </li>`
+     </li>`;
 
-     return markup
- }).join('');
+      return markup;
+    })
+    .join('');
 
- return bookCard;
+  return bookCard;
 }
 
 //// Додавання акцентного кольолру до заголовку з назвою категорії списку книг
 function addColorToTitle() {
-// const textgalleryTitle = galleryTitle.innerHTML;
-const categoryValue = varWithActiveValueFilter.innerHTML;
+  // const textgalleryTitle = galleryTitle.innerHTML;
+  const categoryValue = varWithActiveValueFilter.innerHTML;
 
-let wordsArray = categoryValue.split(" ");
-let lastWord = wordsArray.pop();
-let firstPart = wordsArray.join(" ");
+  let wordsArray = categoryValue.split(' ');
+  let lastWord = wordsArray.pop();
+  let firstPart = wordsArray.join(' ');
 
-galleryTitle.innerHTML = `${firstPart} <span class="books-gallery__title-accent">${lastWord}</span>`;
+  galleryTitle.innerHTML = `${firstPart} <span class="books-gallery__title-accent">${lastWord}</span>`;
 }
 
 function onBtnOpen(evt) {
@@ -131,8 +131,9 @@ function onBtnOpen(evt) {
 function addModal() {
   const booksGalleryCards = document.querySelectorAll('.books-gallery__card');
 
-  booksGalleryCards.forEach((card) => {
-      card.addEventListener('click', onBtnOpen) 
-  })
+  booksGalleryCards.forEach(card => {
+    card.addEventListener('click', onBtnOpen);
+  });
 }
 
+export { addGalleryMarkupAndChangeFilter };
--- src/js/gallery-categories.js
+++ src/js/gallery-categories.js
@@ -35,7 +35,6 @@ function createBooklistMarcup(data) {
     const bookCards = [];
 
     booksArr.forEach(({ _id, book_image, title, author }) => {
-      
       //  Перевірка чи пришла обложка книги з бекенду і заміна її на заглушку в разі необхідності //
       if (!book_image) {
         book_image = '../images/book_plug.jpg';
@@ -80,71 +79,88 @@ const eventLister = document.querySelector('.books-gallery');
 let categoryValue = 'ALL CATEGORIES';
 
 //// Рендер списку книг по категорії при кліку на "see more" кнопці
-eventLister.addEventListener('click', onMoreBtnClick)
+eventLister.addEventListener('click', onMoreBtnClick);
 function onMoreBtnClick(e) {
-    if (e.target.localName === 'button') {
-        categoryValue = e.target.getAttribute('id');
-        
-        addCardsByCategory();
-        // addColorToCategory();
-    } 
-} 
+  if (e.target.localName === 'button') {
+    categoryValue = e.target.getAttribute('id');
+
+    addCardsByCategory();
+    // addColorToCategory();
+    changeColorTitle(categoryValue);
+  }
+}
 
 //// Рендер карток книжок по категоріям
-function addCardsByCategory() {  
-     getBooksCategory(categoryValue).then((booksArr) => {
-        if (!booksArr.length) {
-            Notify.failure(`Sorry, there are no ${categoryValue} books. Please choose another category.`);
-            return;
-        }
-        
-        galleryTitle.innerHTML = categoryValue;
-        booksList.innerHTML = createMoreBooks(booksArr);
+function addCardsByCategory() {
+  getBooksCategory(categoryValue).then(booksArr => {
+    if (!booksArr.length) {
+      Notify.failure(
+        `Sorry, there are no ${categoryValue} books. Please choose another category.`
+      );
+      return;
+    }
 
-        addColorToTitle();
-        addModal();
-    })
+    galleryTitle.innerHTML = categoryValue;
+    booksList.innerHTML = createMoreBooks(booksArr);
+
+    addColorToTitle();
+    addModal();
+  });
 }
 
 //// Створення карток книжок по категоріям
 function createMoreBooks(booksArr) {
-    const bookCard =  booksArr.map(({_id, book_image, title, author}) => {
-        //  Перевірка чи пришла обложка книги з бекенду і заміна її на заглушку в разі необхідності //
-        if (!book_image) {
-            book_image = "../images/book_plug.jpg";
-        }
+  const bookCard = booksArr
+    .map(({ _id, book_image, title, author }) => {
+      //  Перевірка чи пришла обложка книги з бекенду і заміна її на заглушку в разі необхідності //
+      if (!book_image) {
+        book_image = '../images/book_plug.jpg';
+      }
 
-        const markup = `<li id="${_id}" class="books-gallery__card">
+      const markup = `<li id="${_id}" class="books-gallery__card">
         <div class="card-container">
         <img class="books-gallery__card-img" src="${book_image}" alt="${title}" loading="lazy">
         <div class="port-overlay"><p>quick view</p></div>
         </div>
         <h2 class="books-gallery__card-title">${title}</h2>
         <p class="books-gallery__card-author">${author}</p>
-        </li>`
+        </li>`;
 
-        return markup
-    }).join('');
+      return markup;
+    })
+    .join('');
 
-    return bookCard;
+  return bookCard;
 }
 
 //// Додавання акцентного кольолру до заголовку з назвою категорії списку книг
 function addColorToTitle() {
   const textgalleryTitle = galleryTitle.innerHTML;
 
-  let wordsArray = categoryValue.split(" ");
+  let wordsArray = categoryValue.split(' ');
   let lastWord = wordsArray.pop();
-  let firstPart = wordsArray.join(" ");
+  let firstPart = wordsArray.join(' ');
 
   galleryTitle.innerHTML = `${firstPart} <span class="books-gallery__title-accent">${lastWord}</span>`;
 }
 
+function changeColorTitle(title) {
+  const labelCategories = document.querySelectorAll('.filter__item');
+  const activeElement = document.querySelector('.filter__item--active');
+
+  activeElement.classList.remove('filter__item--active');
+  const selectedElement = [...labelCategories].find(({ textContent }) => {
+    return textContent === title;
+  });
+  console.log(selectedElement);
+  selectedElement.classList.add('filter__item--active');
+}
+
 //// Відкриття модального вікна при кліку по картці
 function addModal() {
-    const booksGalleryCards = document.querySelectorAll('.books-gallery__card');
+  const booksGalleryCards = document.querySelectorAll('.books-gallery__card');
 
-    booksGalleryCards.forEach((card) => {
-        card.addEventListener('click', onBtnOpen) 
-    })
+  booksGalleryCards.forEach(card => {
+    card.addEventListener('click', onBtnOpen);
+  });
 }
