fc9662feb56d8951bb7bc52db35efa789ea7d56f
Heorhii Honcharov <0019@ukr.net>
2023-05-17 03:28:27 +0200

    yes  but not finished css

--- src/js/auth-modal.js
+++ src/js/auth-modal.js
@@ -1,3 +1,5 @@
+import { saveLS, loadLS, removeLS } from './storage';
+import { menusToggleOnAuth } from './header';
 // Import needed function from Firebase Authentication SDK and RealTime Database
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, child, get } from 'firebase/database';
@@ -20,7 +22,7 @@ const notifyOptions = {
   position: 'center-top',
 };
 
-// Initialasing Firebase Auth and Database instances
+// Initialasing Firebase Database instances
 const firebaseConfig = {
   apiKey: 'AIzaSyCzgSViH35dPAT3x6h1gt7sQXiu6qtVk-A',
   authDomain: 'book-store-a441d.firebaseapp.com',
@@ -30,95 +32,48 @@ const firebaseConfig = {
   appId: '1:10647209917:web:07621d52f96a30e76ef4d4',
 };
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(
-  app,
-  'https://book-store-a441d-default-rtdb.europe-west1.firebasedatabase.app/'
-);
-const dbRef = ref(
-  getDatabase(
-    app,
-    'https://book-store-a441d-default-rtdb.europe-west1.firebasedatabase.app/'
-  )
-);
 
-// нужно экспортировать функция логаут и функция апдейта базы
-//
-const user = {
+// Объект пользователя. В переменной user.booksArr храним массив айдишников книг.
+let user = {
   name: '',
   photoUrl: './images/png/user.png',
+  userId: '',
+  isSignedIn: false,
   email: '',
   password: '',
   booksArr: [],
-  userId: '',
-  isSignedIn: false,
-
   // Initialasing Firebase Auth
   auth: getAuth(),
+  database: getDatabase(
+    app,
+    'https://book-store-a441d-default-rtdb.europe-west1.firebasedatabase.app/'
+  ),
+  dbRef: ref(
+    getDatabase(
+      app,
+      'https://book-store-a441d-default-rtdb.europe-west1.firebasedatabase.app/'
+    )
+  ),
 };
 
-// Handle Authentication form Modal window
-const authForm = document.querySelector('.js-auth-form');
-const inputName = document.querySelector('.auth-name');
-const buttonChooseForm = document.querySelector('.button-form-choose');
-
-authForm.addEventListener('submit', onSubmitAuthForm);
-buttonChooseForm.addEventListener('click', onClickButtonChooseForm);
-
-//Choosing SignUp or SignIn method autorization
-function onClickButtonChooseForm(event) {
-  if (
-    !event.target.classList.contains('auth-link') ||
-    event.target.classList.contains('button-selected')
-  ) {
-    return;
-  }
-
-  if (event.target.classList.contains('signin')) {
-    inputName.style.display = 'none';
-    authForm.lastElementChild.textContent = 'sign in';
-    event.target.parentElement.firstElementChild.classList.toggle(
-      'button-selected'
-    );
-    event.target.parentElement.lastElementChild.classList.toggle(
-      'button-selected'
-    );
-    return;
-  }
-  event.target.parentElement.firstElementChild.classList.toggle(
-    'button-selected'
-  );
-  event.target.parentElement.lastElementChild.classList.toggle(
-    'button-selected'
-  );
-  inputName.style.display = 'inline-block';
-  authForm.lastElementChild.textContent = 'sign up';
+//--- ключи для локального хранилища.
+const LOCALKEY = 'user';
+const LOGINKEY = 'logged';
+//-------------------------------------------------------------------
+//Local storage engine ---------
+function setUserInLS(object) {
+  saveLS(LOCALKEY, object);
+  saveLS(LOGINKEY, 'true');
 }
 
-//Submit authentication form
-function onSubmitAuthForm(event) {
-  event.preventDefault();
-  const {
-    elements: { name, email, password },
-  } = event.currentTarget;
-  user.name = name.value;
-  user.email = email.value;
-  user.password = password.value;
-  if (inputName.style.display === 'none') {
-    // Check for empty fields
-    if (!user.email.trim() || !user.password.trim) {
-      Notify.failure(`Please complete all fields!`, notifyOptions);
-    }
-    // console.log('signin');
-    signInUser(user);
-    return;
-  }
-  // Check for empty fields
-  if (!user.name.trim() || !user.email.trim || !user.password.trim) {
-    Notify.failure(`Please complete all fields!`, notifyOptions);
-    return;
-  }
-  createUser(user);
+function getUserFromLS() {
+  return loadLS(LOCALKEY);
 }
+
+function isUserSet() {
+  return !!loadLS(LOGINKEY);
+}
+//-------------------------------------------------------------------
 
 // sign up user
 async function createUser({ email, password, auth }) {
@@ -135,14 +90,16 @@ async function createUser({ email, password, auth }) {
       });
       user.userId = userCur.uid;
       user.isSignedIn = true;
-      authForm.reset();
-      backDropClosing();
+      setUserInLS(user);
       Notify.success(`New user ${user.name} created`, notifyOptions);
+      menusToggleOnAuth();
+      return true;
     })
     .catch(error => {
       if (error.message === 'Firebase: Error (auth/email-already-in-use).') {
+        Notify.failure(`This email already in use!`, notifyOptions);
+        return false;
         // Display email fail
-        return Notify.failure(`This email already in use!`, notifyOptions);
       }
     });
 }
@@ -156,64 +113,77 @@ async function signInUser({ email, password, auth }) {
       user.photoUrl = userCur.photoURL;
       user.userId = userCur.uid;
       user.isSignedIn = true;
-      // Get data from the database if user have any stored data
-      if (getUserData(user)) {
-        updateUserDatabase(user);
-      }
-      // Reset authentication form
-      authForm.reset();
-      backDropClosing();
+      getUserData(user);
       Notify.success(`Sign-in successful`, notifyOptions);
+      // Get data from the database
+      setUserInLS(user);
+      menusToggleOnAuth();
+      return true;
     })
     .catch(error => {
       if (error.message === 'Firebase: Error (auth/wrong-password).') {
         // Display email fail
-        return Notify.failure(`Password in wrong direction!`, notifyOptions);
+        Notify.failure(`Password in wrong direction!`, notifyOptions);
+        return false;
       }
       if (error.message === 'Firebase: Error (auth/user-not-found).') {
         // Display email fail
-        return Notify.failure(`Need to be registered first!`, notifyOptions);
+        Notify.failure(`Need to be registered first!`, notifyOptions);
+        return false;
       }
       if (error.code === 'auth/too-many-requests') {
         // Display email fail
-        return Notify.failure(`Too many wrong requests!`, notifyOptions);
+        Notify.failure(`Too many wrong requests!`, notifyOptions);
+        return false;
       }
     });
 }
 
 // sign out user
 function signOutUser({ auth, booksArr }) {
-  if (booksArr) {
-    updateUserDatabase(user);
-  }
+  updateUserDatabase(user);
   signOut(auth)
     .then(() => {
-      user.auth = '';
       user.isSignedIn = false;
-      authForm.reset();
+      menusToggleOnAuth();
+      removeLS(LOGINKEY);
+      removeLS(LOCALKEY);
       Notify.success(`Sign-out successful`, notifyOptions);
+      return true;
     })
     .catch(error => {
       const errorCode = error.code;
       const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      return false;
     });
 }
 
-async function updateUserDatabase({ booksArr }) {
-  await set(ref(database, `users/${user.userId}`), {
-    books: booksArr,
-  });
-  Notify.success(`DataBase updated!`, notifyOptions);
+// Firebase realtime database update
+async function updateUserDatabase({ booksArr, database }) {
+  await set(ref(database, `users/${user.userId}`), booksArr)
+    .then(() => {
+      Notify.success(`DataBase updated!`, notifyOptions);
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      return false;
+    });
 }
 
-async function getUserData({ userId }) {
+// Get firebase realtime database data
+async function getUserData({ userId, dbRef }) {
   await get(child(dbRef, `users/${userId}`))
     .then(snapshot => {
       if (!snapshot.exists()) {
         return true;
       }
       user.booksArr = snapshot.val();
-      // console.log(user.booksArr);
+      setUserInLS(user);
       Notify.success(`Shoplist downloaded from DataBase!`, notifyOptions);
       return false;
     })
@@ -221,57 +191,10 @@ async function getUserData({ userId }) {
       Notify.warning('Error getting data from DataBase', notifyOptions);
       const errorCode = error.code;
       const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
     });
 }
-
-//=============================================================
-// Открытие и закрытие модального окна авторизации
-//==================================================================
-const refs = {
-  bodySelector: document.body,
-  backDropAuth: document.querySelector('.js-overlay-modal'),
-  authModalClose: document.querySelector('.js-modal-close'),
-  //from header
-  authUserLogout: document.querySelector('.btn-header-logout'),
-  authModalOpen: document.querySelector('.btn-header'),
-};
-refs.authModalClose.addEventListener('click', onCrossAuthClose);
-refs.authModalOpen.addEventListener('click', onClickOpenAuthModal);
-refs.authUserLogout.addEventListener('click', onClickOpenAuthLogout);
-function onClickOpenAuthLogout() {
-  signOutUser(user);
-}
-function onClickOpenAuthModal() {
-  document.body.style.position = 'fixed';
-  refs.backDropAuth.classList.toggle('is-hidden');
-  refs.backDropAuth.addEventListener('click', onBackDropClickClose);
-  document.addEventListener('keydown', exitViaEsc);
-}
-function onCrossAuthClose() {
-  backDropClosing();
-}
-function onBackDropClickClose(event) {
-  if (event.target.classList.contains('js-overlay-modal')) {
-    backDropClosing();
-  }
-}
-function exitViaEsc(event) {
-  if (event.key === 'Escape') {
-    backDropClosing();
-  }
-}
-function backDropClosing() {
-  document.body.style.position = '';
-  refs.backDropAuth.classList.toggle('is-hidden');
-  refs.backDropAuth.removeEventListener('click', onBackDropClickClose);
-  document.removeEventListener('keydown', exitViaEsc);
-  // Reseting Authentication form to SignUp state
-  buttonChooseForm.firstElementChild.classList.add('button-selected');
-  buttonChooseForm.lastElementChild.classList.remove('button-selected');
-  inputName.style.display = 'inline-block';
-  authForm.lastElementChild.textContent = 'sign up';
-}
-//------------------------------------------------------------------
 
 //Импорт для хидера - объект User (используется его поля 'user.isSignedIn' (булеан)-
 //проверка на залогиненность, 'user.name' (строка) - имя пользователя) и функция 'signOutUser(user)'
@@ -280,4 +203,14 @@ function backDropClosing() {
 //функции: 'updateUserDatabase(user)' - обновление базы в облаке (из массива - ('user.booksArr')),
 //'getUserData(user)' получение данных из облака в массив - ('user.booksArr')
 
-export { user, signOutUser, updateUserDatabase, getUserData };
+export {
+  user,
+  createUser,
+  signInUser,
+  signOutUser,
+  setUserInLS,
+  getUserFromLS,
+  isUserSet,
+  updateUserDatabase,
+  getUserData,
+};
